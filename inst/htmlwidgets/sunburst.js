ecebd2f693038f6532a5f90a5681be7797d5c33d
Zeyad Deeb <thelostscientist@users.noreply.github.com>
2016-07-23 11:24:58 -0500

    Update sunburst.js

--- inst/htmlwidgets/sunburst.js
+++ inst/htmlwidgets/sunburst.js
@@ -19,7 +19,7 @@ HTMLWidgets.widget({
       
       var margin = { top: 30, right: 30, bottom: 30, left: 30 };
       var width = el.getBoundingClientRect().width;
-      var height = el.getBoundingClientRect().height - margin.top - margin.bottom;
+      var height = el.getBoundingClientRect().height;
       var radius = Math.min(width, height) / 2;
       // Dimensions of sunburst
       //var width = el.getBoundingClientRect().width ;
@@ -94,7 +94,7 @@ HTMLWidgets.widget({
           .attr("height", height + margin.top + margin.bottom)
           .append("g")
           .attr("id", el.id + "-container")
-          .attr("transform", "translate(" + (width + margin.left) / 2 + "," + (height + margin.top) / 2 + ")");
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
       var partition = d3.layout.partition()
           .size([2 * Math.PI, radius * radius])
